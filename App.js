017bc1a36457e39c689be462f8beb087c7295a04
TamirGeron <tamir1234432@gmail.com>
2022-07-11 17:00:27 +0300

    navigation

--- App.js
+++ App.js
@@ -12,43 +12,60 @@ import {
   Button,
   Platform,
   StatusBar,
-  ImageBackground
-} from 'react-native';
+  ImageBackground,
+} from "react-native";
 
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import ViewImageScreen from './src/screens/ViewImageScreen';
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import ViewImageScreen from "./src/screens/ViewImageScreen";
 // import AppNavigator from './src/app.navigator';
-import Homepage from './src/screens/Homepage';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Homepage from "./src/screens/Homepage";
+import Signup from "./src/screens/Signup";
 
+const Stack = createNativeStackNavigator();
 
+// const AppNavigator = StackNavigator({
+//   Homepage: { screen: Homepage },
+//   Signup: { screen: Signup },
+// });
 
 export default function App() {
-  const { landscape } = useDeviceOrientation()
+  const { landscape } = useDeviceOrientation();
 
   const handlePress = () => {
-    console.log('Pressed');
-  }
-
+    console.log("Pressed");
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Homepage} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+      {/* <SafeAreaView style={styles.container}> */}
       {/* <WelcomeScreen /> */}
       {/* <ViewImageScreen /> */}
       {/* <AppNavigator /> */}
-      <Homepage />
-    </SafeAreaView>
+      {/* <Homepage /> */}
+      {/* <AppNavigator /> */}
+      {/* </SafeAreaView> */}
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   img: {
-    width: '100%',
-    height: '100%'
-  }
+    width: "100%",
+    height: "100%",
+  },
 });
---
+++ src/screens/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View,Text } from 'react-native-web';
+
+function Signup() {
+    return (
+        <View>
+            <Text>signup</Text>
+        </View>
+    );
+}
+
+// const styles = StyleSheet.create({
+    
+// })
+
+export default Signup;
