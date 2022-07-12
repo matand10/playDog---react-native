6ebfa746d9ac6dc29df12c0235162d2a8fbd4bf4
Matan Damary <matandamary10@gmail.com>
2022-07-12 07:59:02 +0300

    removing api

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Callout, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export function Map() {
     return (
-        <View style={styles.container}>
-            <MapView style={styles.map} />
-        </View>
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 32.109333,
+                longitude: 34.855499,
+                latitudeDelta: 1,
+                longitudeDelta: 1,
+            }}
+        >
+            <Marker coordinate={{
+                latitude: 32.109333,
+                longitude: 34.855499,
+            }}
+                pinColor="blue"
+            >
+                <Callout>
+                    <Text>
+                        Hello
+                    </Text>
+                </Callout>
+            </Marker>
+        </MapView>
     );
 }
 
